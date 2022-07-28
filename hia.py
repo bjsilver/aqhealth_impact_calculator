4c117787210459e2d69c413f0408a6748addbb55
bjsilver <eebjs@leeds.ac.uk>
2022-07-28 15:00:22 +0100

    modify hia.py so that it accepts a path to a config.yaml file from the console as a sys.argv

--- hia.py
+++ hia.py
@@ -8,44 +8,36 @@ Created on Mon Nov 22 13:34:36 2021
 
 import yaml
 import os
+import sys
 
-SCENARIO_NAME = 'vandonk_2005'
-yamlfile = './config_files/'+SCENARIO_NAME+'.yml'
+config = yaml.safe_load(open(sys.argv[1]))
 
-# load config file
-config = yaml.safe_load(open(yamlfile))
 
-# make a folder for results if there isn't on
-results_fpath = './results/'+SCENARIO_NAME
-if not os.path.exists(results_fpath):
-    os.mkdir(results_fpath)
-
-#%%
-if __name__ == '__main__':
-
-    print('Starting health impact assessment for:', SCENARIO_NAME)
-
-#%% 1. make common grid based on input model data
+def hia():
+    
+    print('Starting health impact assessment for:', config['scenario_name'])    
+    
+    #%% 1. make common grid based on input model data
     print('1: making common grid')
     from make_common_grid import make_common_grid
     make_common_grid()
     print('')
-
-#%% 2. regrid population or model data to common grid
+    
+    #%% 2. regrid population or model data to common grid
     print('2: regridding population data')
     if config['regrid_to'] == 'mod':
-        
         from regrid_population_count import regrid_population_count
         regrid_population_count()
-        
-        
     elif config['regrid_to'] == 'pop':
         from regrid_model_to_popcount import regrid_model_to_popcount
         regrid_model_to_popcount()
     print('')
     
-#%% perform HIA
+    #%% perform HIA
     print('3. Health Impact assessment')
     from hia_calculation import hia_calculation
     hia_calculation()
     print('')
+
+if __name__ == "__main__":
+    hia()
