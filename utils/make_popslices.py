a0bdf0869e5860da0865f2180eec8614c0dcee08
bjsilver <eebjs@leeds.ac.uk>
2022-07-28 17:06:43 +0100

    add comments to make_popslices

--- utils/make_popslices.py
+++ utils/make_popslices.py
@@ -13,6 +13,7 @@ from tqdm import tqdm
 import pickle
 import xarray as xr
 
+# get mapper for isocode to country name
 countries_lookup = pd.read_csv('./lookups/country_lookup.csv',
                                index_col='ISOCODE').squeeze('columns')
 
@@ -20,7 +21,7 @@ popds = load_popds()
 
 countries = popds['National Identifier Grid, v4.11 (2010): National Identifier Grid']
 
-#%%
+#%% load the 5-yearly population data for 2000 to 2020
 das = []
 for year in list(range(2000,2025,5)):
     
@@ -31,25 +32,26 @@ for year in list(range(2000,2025,5)):
 
 
 
-#%%
+#%% create a dataset to store the results
 popds = xr.concat(das, dim='year')
 popds = popds.assign_coords({'year':list(range(2000,2025,5))})
 
-#%%
+#%% interpolate for every year
 
 das = []
 for year in range(2000, 2021):
     print('interpolating', year)
-    if year in popds.year:
+    if year in popds.year: # do not interpolate if year already in dataset
         da = popds.loc[{'year':year}]
     else:
-        da = popds.interp({'year':year})
+        da = popds.interp({'year':year}) # interpolate
     das.append(da)
-            
+
+# concatenate intepolated data into single dataset
 popds = xr.concat(das, dim='year')
 
 
-#%%
+#%% make slices for each year, country
     
 for year in range(2000, 2021):
 
@@ -57,11 +59,12 @@ for year in range(2000, 2021):
 
     popcount = popds.loc[{'year':year}]
         
-    das = {}
+    das = {} #store slices in this dictonary
     for country_isocode in tqdm(countries_lookup.index):
         
         cvalue = countries_lookup.loc[country_isocode]
         
+        # mask using the national identifier grid from GPW
         country_popda = popcount.where(countries == cvalue)
         country_popda.name = country_isocode
         
@@ -73,7 +76,8 @@ for year in range(2000, 2021):
         country_popda = country_popda.drop('year')
         
         das[country_isocode] = country_popda
-        
+    
+    # pickle the dictionary
     with open(f'./grids/pop_count_slices_{year}.P', 'wb') as handle:
         pickle.dump(das, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
